4fe71938cf8c6e666a3d1f8f33a1cc573bb5bba6
weibochen <chenwb66@163.com>
2016-09-19 09:24:08 +0800

    统计元音字母
    输入一个字符串，统计处其中元音字母的数量，并统计出每个元音字母的数量。不区分大小写

---
+++ find count of char in string python.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+vowel_char = ['a', 'e', 'i', 'o', 'u']
+a = input('请输入英文单词：')
+a = a.lower()
+num = 0
+for i in a:
+	if i in vowel_char:
+		num += 1
+print(num)
+
+for b in vowel_char:
+	print(b, ':', a.count(b))
