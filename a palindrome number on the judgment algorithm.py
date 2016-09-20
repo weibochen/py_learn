da0691e664531660e12b2a5b8e5bf96ae43f7e3d
weibochen <chenwb66@163.com>
2016-09-20 14:24:35 +0800

    回文判断
    增加了一种方法解决

--- a palindrome number on the judgment algorithm.py
+++ a palindrome number on the judgment algorithm.py
@@ -1,4 +1,8 @@
 # ---*---coding=utf-8---*---
+
+#第一种方法
+#取右半部分字符反转后与左半部分比较判断
+
 char = input("请输入字符串: ")
 num = len(char)//2
 if len(char)%2 == 1:
@@ -11,3 +15,18 @@ else:
         print('回文')
     else:
         print('不是回文')
+		
+#第二种方法
+#依次取出每个字符进行比较，若有一个不同，便中断程序
+
+char = input("请输入字符串: ")
+num = len(char)//2
+for i in range(num):
+	if char[i] != char[-1-i]:
+		print('不是回文')
+		break
+	elif i == num-1:
+		print('回文')
+	else:
+		pass
+
