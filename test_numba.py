00e29f38cbf4d557cf4f7085a5606a60a3eb817b
weibochen <weber1892@outlook.com>
2019-05-17 21:00:09 +0800

    numba library: change py code to C

---
+++ test_numba.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+from numba import jit, njit
+import sys
+import time
+
+
+@njit
+def test(num):
+    ss = 0
+    for i in range(num):
+        ss += 2**i
+    return ss
+
+
+if __name__ == "__main__":
+    start = time.time()
+    num = int(sys.argv[1])
+    ss = test(num)
+    print(ss)
+    end = time.time()
+    print('Run time: ', round(end - start, 2))
